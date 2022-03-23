d9968609e1a1828f5677842b590b98d75491e783
TamraPeterson <peterson.tamra@gmail.com>
2022-03-23 14:22:00 -0600

    sdafa

---
+++ bookNook/server/controllers/BooksController.js
@@ -0,0 +1,8 @@
+import BaseController from "../utils/BaseController";
+
+export class BooksController extends BaseController {
+  constructor() {
+    super('')
+    this.router
+  }
+}
--- bookNook/server/db/DbContext.js
+++ bookNook/server/db/DbContext.js
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
+import { BookSchema } from '../models/Book'
+
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
+  Books = mongoose.model('Book', BookSchema)
 }
 
 export const dbContext = new DbContext()
