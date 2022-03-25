57c66fbf77dd341a1f634e7556c80118d6d40d5f
TyHafen <tyhafen@gmail.com>
2022-03-25 12:27:16 -0600

    modal works for book details

--- bookNook.client/src/components/BookCover.vue
+++ bookNook.client/src/components/BookCover.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="component">
     <img
-      class="shadow cover-size"
+      data-bs-toggle="modal"
+      data-bs-target="#bookDetails-modal"
+      class="shadow cover-size rounded selectable"
       :src="searchBook.imageLinks.thumbnail"
       alt=""
     />
   </div>
+  <Modal id="bookDetails-modal">
+    <template #modal-title>{{ searchBook.title }}</template>
+    <template #modal-body
+      >Author: {{ searchBook.authors[0] }}
+      {{ searchBook.description }}
+    </template>
+  </Modal>
 </template>
 
 
@@ -29,5 +38,10 @@ export default {
 .cover-size {
   height: 300px;
   width: 200px;
+  transition: 0.3s;
+}
+.cover-size:hover {
+  transform: scale(1.1);
+  transition: 0.3s;
 }
 </style>
--- bookNook.client/src/components/Modal.vue
+++ bookNook.client/src/components/Modal.vue
@@ -1,43 +1,42 @@
 <template>
-      <div
-        class="modal fade"
-        tabindex="-1"
-        role="dialog"
-        aria-labelledby="modelTitleId"
-        aria-hidden="true"
-    >
-        <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content bg-transparent border-0 text-light">
-                <div class="modal-header">
-                    <h5 class="modal-title">
-                        <slot name="title"></slot>
-                    </h5>
-                    <button
-                        type="button"
-                        class="btn-close"
-                        data-bs-dismiss="modal"
-                        aria-label="Close"
-                    ></button>
-                </div>
-                <div class="modal-body">
-                    <slot name="body"></slot>
-                </div>
-                <div class="modal-footer">
-                    <slot name="footer"></slot>
-                </div>
-            </div>
+  <div
+    class="modal fade"
+    id="form-modal"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="modelTitleId"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header bg-secondary">
+          <h5 class="modal-title">
+            <slot name="modal-title"> <!--title slot --></slot>
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
         </div>
+        <div class="modal-body bg-secondary">
+          <slot name="modal-body"><!--body slot --></slot>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
+
 
 <script>
 export default {
-    setup() {
-        return {}
-    }
-}
+  setup() {
+    return {};
+  },
+};
 </script>
 
-<style>
 
+<style lang="scss" scoped>
 </style>
