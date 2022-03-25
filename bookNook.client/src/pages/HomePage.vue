7b1053a3f60b4d5782dc480b61aef43727f61262
Isaiah Snyder <isaiahsnyder.programming@gmail.com>
2022-03-25 16:42:16 -0600

    Sync

--- bookNook.client/src/pages/HomePage.vue
+++ bookNook.client/src/pages/HomePage.vue
@@ -45,6 +45,7 @@
 <script>
 import { onMounted } from '@vue/runtime-core';
 import { useRouter } from 'vue-router';
+import { logger } from '../utils/Logger';
 
 export default {
   name: 'Home',
@@ -55,15 +56,14 @@ export default {
       showSlides()
     })
     function showSlides() {
-      let i;
       let slides = document.getElementsByClassName("mySlides");
-      for (i = 0; i < slides.length; i++) {
+      for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
       }
       slideIndex++;
       if (slideIndex > slides.length) { slideIndex = 1 }
       slides[slideIndex - 1].style.display = "block";
-      setTimeout(showSlides, 10000)
+      setTimeout(showSlides, 5000)
     }
     return {
       goTo(page) {
