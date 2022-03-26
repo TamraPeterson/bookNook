93960f46f669a824466aa329ab869fd99864d00c
Isaiah Snyder <isaiahsnyder.programming@gmail.com>
2022-03-26 17:42:03 -0600

    Removed Slideshow

--- bookNook.client/src/pages/HomePage.vue
+++ bookNook.client/src/pages/HomePage.vue
@@ -2,23 +2,8 @@
 <div class="hide-overflow">
 
   <div class="slideshow-container">
-    <!-- Full-width images with number and caption text -->
-    <div class="mySlides fade">
-      <div class="numbertext">1 / 3</div>
-      <img src="https://i.redd.it/0yndtkv4wsg51.jpg" style="width: 100%" class="landing-img"/>
-      <div class="text">Caption Text</div>
-    </div>
-
-    <div class="mySlides fade">
-      <div class="numbertext">2 / 3</div>
+    <div class="">
       <img src="src/assets/img/Ladder.jpg" style="width: 100%" class="landing-img"/>
-      <div class="text">Caption Two</div>
-    </div>
-
-    <div class="mySlides fade">
-      <div class="numbertext">3 / 3</div>
-      <img src="src/assets/img/BookPathway.jpg" style="width: 100%" class="landing-img"/>
-      <div class="text">Caption Three</div>
     </div>
 
     <div class="quote-text text-light rounded-2">
@@ -26,148 +11,28 @@
       <p>-Dr. Seuss</p>
     </div>
     <a class="text-white btn bg-micks-hat rounded-pill fw-bold p-2 px-4 mt-2 button-center" @click="goTo('Books')">Start Your Next Adventure</a>
-
-    <!-- Next and previous buttons -->
-    <!-- <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-    <a class="next" onclick="plusSlides(1)">&#10095;</a> -->
   </div>
-  <br />
-
-  <!-- The dots/circles -->
-  <!-- <div style="text-align: center">
-    <span class="dot" onclick="currentSlide(1)"></span>
-    <span class="dot" onclick="currentSlide(2)"></span>
-    <span class="dot" onclick="currentSlide(3)"></span>
-  </div> -->
-  </div>
+</div>
 </template>
 
 <script>
-import { onMounted } from '@vue/runtime-core';
-import { useRouter } from 'vue-router';
-import { logger } from '../utils/Logger';
-
 export default {
-  name: 'Home',
-  setup() {
-    let slideIndex = 0
-    const router = useRouter()
-    onMounted(async () => {
-      showSlides()
-    })
-    function showSlides() {
-      let slides = document.getElementsByClassName("mySlides");
-      for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) { slideIndex = 1 }
-      slides[slideIndex - 1].style.display = "block";
-      setTimeout(showSlides, 5000)
-    }
-    return {
-      goTo(page) {
-        router.push({ name: page })
-      }
-    }
-  }
+  name: 'Home'
 }
 </script>
 
 <style scoped lang="scss">
 .hide-overflow{
   overflow: hidden;
-  max-height: 87vh;
+  max-height: 88.4vh;
 }
 
 * {box-sizing:border-box}
 
-/* Slideshow container */
 .slideshow-container {
-  // overflow: hidden;
-  min-height: 87vh;
+  min-height: 88.4vh;
   max-width: 100vw;
   position: relative;
-  // margin: auto;
-}
-
-/* Hide the images by default */
-.mySlides {
-  display: none;
-  height: 87vh;
-  img{
-    height: 87vh;
-  }
-}
-
-/* Next & previous buttons */
-.prev, .next {
-  cursor: pointer;
-  position: absolute;
-  top: 50%;
-  width: auto;
-  margin-top: -22px;
-  padding: 16px;
-  color: white;
-  font-weight: bold;
-  font-size: 18px;
-  transition: 0.6s ease;
-  border-radius: 0 3px 3px 0;
-  user-select: none;
-}
-
-/* Position the "next button" to the right */
-.next {
-  right: 0;
-  border-radius: 3px 0 0 3px;
-}
-
-/* On hover, add a black background color with a little bit see-through */
-.prev:hover, .next:hover {
-  background-color: rgba(0,0,0,0.8);
-}
-
-/* Caption text */
-.text {
-  position: fixed;
-  text-shadow: 0px 0px 4px rgba(0, 0, 0, 0.466);
-  color: #f2f2f2;
-  font-size: 15px;
-  padding: 8px 12px;
-  bottom: 8px ;
-  width: 100%;
-  text-align: center;
-}
-
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}
-
-/* The dots/bullets/indicators */
-.dot {
-  cursor: pointer;
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
-
-.active, .dot:hover {
-  background-color: #717171;
-}
-
-/* Fading animation */
-.fade {
-  animation-name: fade;
-  animation-duration: 5s;
 }
 
 .quote-text {
@@ -190,11 +55,7 @@ export default {
 }
 
 .button-center {
-    // background-color: #111927;
-    // background-color: rgba(17, 25, 39, 0.4);
-    // color: #e9ecef;
     font-weight: bold;
-    // border: 3px solid #e9ecef;
     position: absolute;
     top: 66%;
     left: 50%;
@@ -222,10 +83,5 @@ export default {
     width: 25%;
     top: 70%;
   }
-}
-
-@keyframes fade {
-  from {opacity: .4}
-  to {opacity: 1}
 }
 </style>
