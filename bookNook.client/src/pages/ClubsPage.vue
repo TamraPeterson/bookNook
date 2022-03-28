8a9d8c463abd220924aaa32423d89cabb1e14618
TyHafen <tyhafen@gmail.com>
2022-03-28 16:26:51 -0600

    starting clubs page

--- bookNook.client/src/pages/ClubsPage.vue
+++ bookNook.client/src/pages/ClubsPage.vue
@@ -1,43 +1,27 @@
 <template>
   <div class="container-fluid bg-light">
-    <div class="row mt-1">
-      <div class="col-md-4"></div>
-      <div class="col-md-4 mt-2 text-center"><ClubSearch /></div>
-      <div class="col-md-4"></div>
-      <div class="row mt-5 p-2 justify-content-center">
-        <div
-          v-for="c in searchClubs"
-          :key="c.id"
-          class="col-md-2 m-3 d-flex justify-content-center"
-        >
-          <ClubButton :searchClub="c" />
-        </div>
-      </div>
-    </div>
     <div class="col-6">
       <i
         v-if="account.id"
         data-bs-toggle="modal"
         data-bs-target="#club-modal"
         class="mdi mdi-pencil selectable"
-      >Create Club</i>
+        >Create Club</i
+      >
     </div>
   </div>
 
-
   <Modal id="club-modal">
     <template #modal-title>
-        <h1>Create Club Form</h1>
+      <h1>Create Club Form</h1>
     </template>
 
     <template #modal-body>
-    <form class="row d-flex flex-column bg-dark p-3">
-      
-        <!-- <h5 class="p-1 pt-3">Name:</h5>
-        <input v-model="editable.name" type="text"> -->
-
-      <button type="button" class="btn btn-info" @click="update">Submit</button>
-    </form>
+      <form class="row d-flex flex-column bg-dark p-3">
+        <button type="button" class="btn btn-info" @click="update">
+          Submit
+        </button>
+      </form>
     </template>
   </Modal>
 </template>
