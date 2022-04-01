6d141fdd51ae2aa6e775b75e57b2602214a5840b
TamraPeterson <peterson.tamra@gmail.com>
2022-04-01 15:07:58 -0600

    comments work bruv

--- bookNook.client/src/services/CommentsService.js
+++ bookNook.client/src/services/CommentsService.js
@@ -1,5 +1,6 @@
 import { AppState } from "../AppState"
 import { logger } from "../utils/Logger"
+import Pop from "../utils/Pop"
 import { api } from "./AxiosService"
 
 class CommentsService {
@@ -12,8 +13,21 @@ class CommentsService {
     async newComment(comment) {
         const res = await api.post('api/comments', comment)
         logger.log('comment created', res.data)
+        AppState.comments.push(res.data)
+    }
+
+    async deleteComment(id) {
+
+        if (await Pop.confirm("are you sure?")) {
+            const comment = AppState.comments.find(c => c.id == id)
+            logger.log('this be  id', id)
+            await api.delete('api/comments/' + comment.id)
+            AppState.comments = AppState.comments.filter(c => c != comment)
+        }
+
 
     }
 }
+
 
 export const commentsService = new CommentsService()
